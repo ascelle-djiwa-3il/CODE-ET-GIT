08ed390a29a0107fa85963178c0846cb87e9d96d
DJIWA <djiwatca@3il.fr>
2024-11-26 18:23:27 +0100

    Correction de l'exercice 3

--- exercice_3/exercice3.java
+++ exercice_3/exercice3.java
@@ -1,22 +1,47 @@
 public class StockManager {
 
-    // gere le stock
+    /**Gere le stock
+     * @param type le type de l'opération
+     * @param type la dénomination du produit
+     * @param type la quantité du produit commandée
+     * @param type la quantité du produit en stock
+     * */ 
     public void gererStock(String typeOperation, String produit, int quantite, int stock) {
-        // Ajout au stock
-        if (typeOperation.equals("ajout")) {
-            stock += quantite;
-            System.out.println("Produit : " + produit + ", Stock après ajout : " + stock);
-        // Retrait du stock
-        } else if (typeOperation.equals("retrait")) {
-            if (stock >= quantite) {
-                stock -= quantite;
+        
+        Switch (typeOperation){
+            case "ajout": // Ajout au stock
+                ajouterStock(stock,quantite);
+                System.out.println("Produit : " + produit + ", Stock après ajout : " + stock);
+            case "retrait": // Retrait du stock
+                retraitStock(stock,quantite);
                 System.out.println("Produit : " + produit + ", Stock après retrait : " + stock);
-            } else {
-                System.out.println("Stock insuffisant pour le produit : " + produit);
-            }
-        // Si l'action est inconnu
-        } else {
-            System.out.println("Opération inconnue.");
+            default: // Si l'action est inconnu
+                System.out.println("Opération inconnue.");
+        }
+        
+    }
+
+    /**Ajoute un produit au stock
+     * @param type la quantité du produit en stock
+     * @param type la quantité du produit commandée
+     */ 
+    public void ajouterStock(int stock, int quantite){
+        stock += quantite;
+    }
+
+
+    /**Retire un produit du stock
+     * @param type la quantité du produit en stock
+     * @param type la quantité du produit commandée(à retirer)
+     */ 
+    public void retraitStock(int stock, int quantite){
+        if(stock>=quantite){
+            stock-=quantite;
+        }else{
+            System.out.println("Stock insuffisant!");
         }
     }
+
+
 }
+
